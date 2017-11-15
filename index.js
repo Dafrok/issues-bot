0b022a73e33f407760e7b075776a7b1f518b74d2
Dafrok <o.o@mug.dog>
2017-11-15 10:46:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', function (req, res) {
+  res.send('Webhook');
+});
+
+app.listen(port, function () {
+  console.log(`Listening port ${port}...`);
+});
